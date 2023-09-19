68481ce69050eabbf2916f272f1c7c63fd10fccb
Mohammad JafariFard <jafarifard@hotmail.com>
2023-09-19 15:41:44 +0330

    Sent 2file to git

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//--To see the result, please press the 
+//--  ّ12 keys in the console section
+//-------------------------------------------------
+//Using JavaScript, we want to know which of the
+// numbers in the array are even and which are odd
+
+const number = [13, 23, 12, 45, 22, 48, 66, 100];
+for (i = 0; number[i] <= number[i]; i++) {
+    if (number[i] % 2 === 0)
+     { console.log(number[i], "is Even"); }
+     else {console.log(number[i], "is Odd");}
+}
+//-------------------------------------------
